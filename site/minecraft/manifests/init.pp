f1e6b10827e42a6e4e2718eefd7be9860764362c
Art Balingit <42024469+balingit@users.noreply.github.com>
2022-05-13 19:39:10 +1200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,8 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source      => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
@@ -20,5 +21,6 @@ class minecraft {
   service { 'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
   }
 }
